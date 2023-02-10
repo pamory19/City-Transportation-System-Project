51b6f881d62bb163f55468a2cfcc410c8d6d4738
Paris Amory <parisamory@Pariss-MacBook-Pro.local>
2023-02-09 22:08:49 -0800

    Paris - added maven, dependencies, and logger

---
+++ src/main/java/com/solvd/citytransportationsystemproject/Main.java
@@ -0,0 +1,16 @@
+package com.solvd.citytransportationsystemproject;
+
+import org.apache.logging.log4j.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main {
+    private static final Logger logger = LogManager.getLogger(Main.class);
+
+    public static void main( String[] args )
+    {
+        logger.info( "Hello World!" );
+    }
+}
