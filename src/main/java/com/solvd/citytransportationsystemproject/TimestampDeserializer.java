d1fad256711f4a1fc462585649d8438a7a9692fb
efox1515 <122343810+efox1515@users.noreply.github.com>
2023-02-15 22:19:51 -0500

    Add files via upload

---
+++ src/main/java/com/solvd/citytransportationsystemproject/TimestampDeserializer.java
@@ -0,0 +1,35 @@
+package com.solvd.hospitalsystem;
+
+import java.io.IOException;
+import java.sql.Timestamp;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.solvd.hospitalsystem.dao.mysql.AppointmentDiagnosisDAO;
+
+public class TimestampDeserializer extends JsonDeserializer<Timestamp> {
+	final Logger logger = LogManager.getLogger(AppointmentDiagnosisDAO.class.getName());
+
+	private SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+	@Override
+	public Timestamp deserialize(JsonParser parser, DeserializationContext context)
+			throws IOException, JsonProcessingException {
+		String date = parser.getText();
+		try {
+			Date parsedDate = dateFormat.parse(date);
+			return new Timestamp(parsedDate.getTime());
+		} catch (ParseException e) {
+			logger.info(e);
+		}
+		return null;
+	}
+}
